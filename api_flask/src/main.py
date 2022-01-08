6e659800b8ecebb655d55ec6348dd4958cc453d9
Николай Алексеев <asakura475@gmail.com>
2022-01-08 23:00:02 +0300

    Start tests!

--- api_flask/src/main.py
+++ api_flask/src/main.py
@@ -6,8 +6,8 @@ from work_with_db.db_helper import DBHelper
 application = Flask(__name__)
 wsgi_app = application.wsgi_app
 
-# DB
-db_helper = DBHelper(**DBHelper.read_settings_file())
+# # DB
+# db_helper = DBHelper(**DBHelper.read_settings_file())
 
 
 @application.route("/")
@@ -22,22 +22,22 @@ def start_api_page() -> str:
     return "Hello, World!"
 
 
-@application.route("/api/users/", methods=["GET"])
-def get_users() -> Response:
-    return jsonify(db_helper.get_users())
-
-
-@application.route("/api/check_user/", methods=["GET"])
-def check_user() -> Response:
-    return jsonify(
-        [
-            {
-                "answer": db_helper.check_user(
-                    request.args.get("username"), request.args.get("password"), bool(int(request.args.get("is_md5")))
-                )
-            }
-        ]
-    )
+# @application.route("/api/users/", methods=["GET"])
+# def get_users() -> Response:
+#     return jsonify(db_helper.get_users())
+#
+#
+# @application.route("/api/check_user/", methods=["GET"])
+# def check_user() -> Response:
+#     return jsonify(
+#         [
+#             {
+#                 "answer": db_helper.check_user(
+#                     request.args.get("username"), request.args.get("password"), bool(int(request.args.get("is_md5")))
+#                 )
+#             }
+#         ]
+#     )
 
 
 def main() -> None:
